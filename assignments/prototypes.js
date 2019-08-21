5897153a10e620bca41485ee9a41f16b5c503de8
Sandrava Philips <lacasandra1@gmail.com>
2019-08-21 15:03:27 +0100

    this.js Completed

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -167,7 +167,7 @@ function LambdaProject (protypejs, thisjs, challenges) {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -225,4 +225,4 @@ function LambdaProject (protypejs, thisjs, challenges) {
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: the value of "this" will be on the window
+* 2. Implicit Binding: "this" creates an instance of a property of a object.
+* 3. New Binding: "this" creates an instance of the initial constructor. But "new" is used instead.
+* 4. Explicit Binding: occurs when call() or apply() is used. bind() is used to create a new function off an initial function.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,36 @@
 // Principle 1
 
 // code example for Window Binding
+var topLevelVariable = 5;
+console.log(window.topLevelVariable);
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    name: "Sandrava",
+    age: 34,
+    print: function() {
+        return `My name is ${this.name} and I'm ${age} years old.`
+    }
+}
 
 // Principle 3
 
 // code example for New Binding
+function Person(name, age) {
+    this.myName = name;
+    this.myAge = age;
+    this.myStomach = [];
+  }
+
+  const babyConstructor = new Person(name, age);
 
 // Principle 4
 
 // code example for Explicit Binding
+function randomExample () {
+    return this;
+}
+
+randomExample.call('ice-cream');
